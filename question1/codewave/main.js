cff70a1131aff8535937aa4c0762120bc9e8546d
fateme ebrahimzadeh <fateme.ebrahimzadeh20@gmail.com>
2023-05-25 10:48:27 +0330

    add defensive programming concerns to challenge 1

--- question1/codewave/main.js
+++ question1/codewave/main.js
@@ -1,4 +1,18 @@
+const isTypeNumber = (input) => {
+  return typeof input === "number";
+};
+
+const isArrayValid = (nums) => {
+  for (const num of nums) {
+    if (isTypeNumber(num) === false) return false;
+  }
+  return true;
+};
+
 const getPairKeys = (nums, target) => {
+  if (!isArrayValid(nums)) return "array is not valid!";
+  if (!isTypeNumber(target)) return "target is not valid!";
+
   const result = [];
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
@@ -13,3 +27,5 @@ const getPairKeys = (nums, target) => {
 console.log(getPairKeys([2, 7, 11, 15], 9));
 console.log(getPairKeys([3, 2, 4], 6));
 console.log(getPairKeys([3, 3], 6));
+console.log(getPairKeys([3, 3, "h"], 6));
+console.log(getPairKeys([3, 3], "6"));
