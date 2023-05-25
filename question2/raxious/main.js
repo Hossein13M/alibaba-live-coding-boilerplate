4b3e8d982cae70abe7e20d65be8a96774574938f
ahmadreza tahmasbi <ahmadreza.tmb@gmail.com>
2023-05-25 12:13:28 +0330

    [feat] implement basic functionality

--- question2/raxious/main.js
+++ question2/raxious/main.js
@@ -19,3 +19,55 @@ const response = {
         },
     ]
 }
+
+var currentStep = 1;
+
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next')
+const ul = document.querySelector('ul')
+console.log(ul)
+function setQuestion(){
+    questions = response.stepInfo[currentStep -1].questions;
+    console.log(questions);
+    for(let question of questions){
+        const li = document.createElement('li');
+        li.innerHTML = question;
+        ul.appendChild(li);
+    }
+    
+}
+
+function initialQuestions(){
+    setQuestion()
+    updateBtnstatus()
+}
+
+function updateBtnstatus(){
+    if (currentStep == 1){
+        prevBtn.classList.add('disable')
+        nextBtn.classList.remove('disable')
+    }else if (currentStep == response.stepCount){
+        prevBtn.classList.remove('disable')
+        nextBtn.classList.add('disable')
+    }else{
+        prevBtn.classList.remove('disable')
+        nextBtn.classList.remove('disable')
+    }
+}
+
+function updateNextStep(){
+    currentStep++
+    ul.innerText=""
+    setQuestion()
+    updateBtnstatus()
+}
+function updatePrevStep(){
+    currentStep--
+    ul.innerText=""
+    setQuestion()
+    updateBtnstatus()
+}
+
+window.addEventListener('load',initialQuestions ) 
+nextBtn.addEventListener('click', updateNextStep)
+prevBtn.addEventListener('click', updatePrevStep )
